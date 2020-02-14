6c9d03fa6000ff5de1d3500ecd4f9bce2bd10181
BaekGeunYoung <dvmflstm@naver.com>
2020-02-14 16:52:12 +0900

    concert 도메인 객체에 business logic 추가

--- src/main/kotlin/com/practice/concert_reservation_app/domain/concert/domain/Concert.kt
+++ src/main/kotlin/com/practice/concert_reservation_app/domain/concert/domain/Concert.kt
@@ -17,5 +17,28 @@ data class Concert(
         @ElementCollection(fetch = FetchType.EAGER)
         var seats: MutableSet<Seat>? = null
 ) {
-        constructor() : this(concertName = "")
+    constructor() : this(concertName = "")
+
+    fun isValidSeat(seatNumber: Int): Boolean {
+        val targetSeat = seats!!.find { it.seatNumber == seatNumber }
+
+        targetSeat?: return false
+        return true
+    }
+
+    fun canReserve(seatNumber: Int): Boolean {
+        val targetSeat = seats!!.find { it.seatNumber == seatNumber }
+
+        return targetSeat!!.isTaken
+    }
+
+    fun takeSeat(seatNumber: Int) {
+        val targetSeat = seats!!.find { it.seatNumber == seatNumber }
+        targetSeat!!.isTaken = true
+    }
+
+    fun untakeSeat(seatNumber: Int) {
+        val targetSeat = seats!!.find { it.seatNumber == seatNumber }
+        targetSeat!!.isTaken = false
+    }
 }
