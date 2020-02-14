8cc850374b7b09ca1a32b6331473ed8dfa1f1c9e
BaekGeunYoung <dvmflstm@naver.com>
2020-02-14 16:53:48 +0900

    회원가입 시에 코드 레벨에서 username 중복 검사를 하고 custom exception을 발생시키도록 함.

--- src/main/kotlin/com/practice/concert_reservation_app/domain/user/application/impl/UserSignUpServiceImpl.kt
+++ src/main/kotlin/com/practice/concert_reservation_app/domain/user/application/impl/UserSignUpServiceImpl.kt
@@ -3,6 +3,7 @@ package com.practice.concert_reservation_app.domain.user.application.impl
 import com.practice.concert_reservation_app.domain.user.application.UserSignUpService
 import com.practice.concert_reservation_app.domain.user.domain.User
 import com.practice.concert_reservation_app.domain.user.dto.SignUpDto
+import com.practice.concert_reservation_app.domain.user.exception.DuplicateUsernameException
 import com.practice.concert_reservation_app.domain.user.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -14,6 +15,10 @@ class UserSignUpServiceImpl(
     @Autowired private val bCryptPasswordEncoder: BCryptPasswordEncoder
 ): UserSignUpService {
     override fun signUp(signUpReq: SignUpDto.SignUpReq): User {
+        if(userRepository.existsByUsername(signUpReq.username)) {
+            throw DuplicateUsernameException(signUpReq.username)
+        }
+
         val newUser = signUpReq.toEntity(bCryptPasswordEncoder)
         return userRepository.save(newUser)
     }
