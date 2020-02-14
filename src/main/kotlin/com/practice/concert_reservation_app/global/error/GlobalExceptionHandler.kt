e858aee7809c5f4d130b99d0aa39776b0bd89074
BaekGeunYoung <dvmflstm@naver.com>
2020-02-14 12:26:03 +0900

    exception handling을 위한 GlobalExceptionHandler 생성

---
+++ src/main/kotlin/com/practice/concert_reservation_app/global/error/GlobalExceptionHandler.kt
@@ -0,0 +1,8 @@
+package com.practice.concert_reservation_app.global.error
+
+import org.springframework.web.bind.annotation.ControllerAdvice
+
+@ControllerAdvice
+class GlobalExceptionHandler {
+
+}
